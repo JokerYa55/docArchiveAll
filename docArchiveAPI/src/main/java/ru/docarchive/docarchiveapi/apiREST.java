ea136835c3bae690f9264886a33153d32b93841e
vasil <andr_vasil@mail.ru>
2018-02-20 21:11:19 +0300

    Добавлена работа с СМС подтверждением

--- docArchiveAPI/src/main/java/ru/docarchive/docarchiveapi/apiREST.java
+++ docArchiveAPI/src/main/java/ru/docarchive/docarchiveapi/apiREST.java
@@ -5,9 +5,13 @@
  */
 package ru.docarchive.docarchiveapi;
 
+import java.util.List;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -15,9 +19,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
 import rtk.docarchive.dao.beans.TProject;
-
-
 
 /**
  *
@@ -29,24 +32,52 @@ import rtk.docarchive.dao.beans.TProject;
 public class apiREST {
 
     Logger log = Logger.getLogger(getClass().getName());
-    
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+
     public apiREST() {
         log.info("Constructor");
     }
-    
+
+    private EntityManager getEM() {
+        if (this.emf == null) {
+            this.emf = Persistence.createEntityManagerFactory("docArchiveAPI_JPA");
+        }
+        this.em = this.emf.createEntityManager();
+        return this.em;
+    }
+
     @Path("/test")
     @GET
     @RolesAllowed("doc-archive-user")
     public String test() {
         return "test";
     }
-    
-    @Path("/addProject")
+
+    @Path("/project")
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed("doc-archive-user")    
+    @RolesAllowed("doc-archive-user")
     public Response addProject(TProject project) {
         return Response.status(Response.Status.CREATED).build();
     }
-    
+
+    @Path("/project")
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @RolesAllowed("doc-archive-user")
+    public Response getProject() {
+        List<TProject> res = null;
+        try {
+            getEM();
+            em.getTransaction().begin();
+            res = em.createQuery("from TProject u").getResultList();
+            log.info(String.format("res = %s", res));
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            log.log(Priority.ERROR, e);
+        }
+        return Response.status(Response.Status.OK).entity(res).build();
+    }
+
 }
